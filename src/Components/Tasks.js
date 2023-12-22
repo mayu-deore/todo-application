4b9e9c56d2a2897ccab3517071f65300a87b53a9
Mayuri Deore <deoremayuri39@gmail.com>
2023-12-22 17:08:57 +0530

    Validations and Styling

--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -1,4 +1,3 @@
-// Tasks.js
 import React, { useState } from 'react';
 import './Tasks.css';
 
@@ -17,15 +16,15 @@ const Tasks = () => {
   const handleAddTask = () => {
     if (newTask.title.trim() !== '' && newTask.description.trim() !== '' && newTask.startDate.trim() !== '') {
       if (editingTaskId !== null) {
-        // Update existing task
+
         setTasks(tasks.map((task) => (task.id === editingTaskId ? { ...task, ...newTask } : task)));
         setEditingTaskId(null);
       } else {
-        // Add new task
+
         setTasks([...tasks, { ...newTask, id: Date.now() }]);
       }
 
-      // Clear the form
+
       setNewTask({
         title: '',
         description: '',
@@ -60,78 +59,95 @@ const Tasks = () => {
     <div className="app">
       <h1>ToDo App</h1>
       <div>
-        <input
-          type="text"
-          placeholder="Title"
-          value={newTask.title}
-          onChange={(e) => setNewTask({ ...newTask, title: e.target.value })}
-        />
-        <input
-          type="text"
-          placeholder="Description"
-          value={newTask.description}
-          onChange={(e) => setNewTask({ ...newTask, description: e.target.value })}
-        />
-        <input
-          type="date"
-          value={newTask.startDate}
-          onChange={(e) => setNewTask({ ...newTask, startDate: e.target.value })}
-        />
+        <label>Task Title
+
+          <input
+            type="text"
+            placeholder="Title"
+            value={newTask.title}
+            onChange={(e) => setNewTask({ ...newTask, title: e.target.value })}
+          />
+        </label>
+        <label>Task Description
+
+          <input
+            type="text"
+            placeholder="Description"
+            value={newTask.description}
+            onChange={(e) => setNewTask({ ...newTask, description: e.target.value })}
+          />
+        </label>
+        <label>Select the Start Date of Task
+
+          <input
+            type="date"
+            value={newTask.startDate}
+            onChange={(e) => setNewTask({ ...newTask, startDate: e.target.value })}
+          />
+        </label>
         <button onClick={handleAddTask}>{editingTaskId !== null ? 'Update Task' : 'Add Task'}</button>
       </div>
       <div>
         <h2>Tasks</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Description</th>
-              <th>Start Date</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tasks.map((task) => (
-              <tr key={task.id}>
-                <td>{task.title}</td>
-                <td>{task.description}</td>
-                <td>{task.startDate}</td>
-                <td style={{display:"flex", gap:"20px"}}>
-                  <button style={{backgroundColor:"blue"}} onClick={() => handleEditTask(task.id, task)}>Edit</button>
-                  <button style={{backgroundColor:"red"}} onClick={() => handleDeleteTask(task.id)}>Delete</button>
-                  <button onClick={() => handleCompleteTask(task.id)}>Mark as Completed</button>
-                </td>
+        {tasks.length === 0 ? (
+          <p>No tasks to display.</p>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Start Date</th>
+                <th>Actions</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {tasks.map((task) => (
+                <tr key={task.id}>
+                  <td>{task.title}</td>
+                  <td>{task.description}</td>
+                  <td>{task.startDate}</td>
+                  <td style={{ display: "flex", gap: "20px" }}>
+                    <button style={{ backgroundColor: "blue" }} onClick={() => handleEditTask(task.id, task)}>Edit</button>
+                    <button style={{ backgroundColor: "red" }} onClick={() => handleDeleteTask(task.id)}>Delete</button>
+                    <button onClick={() => handleCompleteTask(task.id)}>Mark as Completed</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
       <div className="completed-tasks">
         <h2>Completed Tasks</h2>
-        <table>
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Description</th>
-              <th>Start Date</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {completedTasks.map((task) => (
-              <tr key={task.id}>
-                <td>{task.title}</td>
-                <td>{task.description}</td>
-                <td>{task.startDate}</td>
-                <td style={{display:"flex", gap:"20px"}}>
-                  <button style={{backgroundColor:"blue"}} onClick={() => handleEditTask(task.id, task)}>Edit</button>
-                  <button style={{backgroundColor:"red"}} onClick={() => handleDeleteTask(task.id)}>Delete</button>
-                  <button onClick={() => handleIncompleteTask(task.id)}>Mark as Incomplete</button>
-                </td>
+        {completedTasks.length === 0 ? (
+          <p>No completed tasks to display.</p>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Description</th>
+                <th>Start Date</th>
+                <th>Actions</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {completedTasks.map((task) => (
+                <tr key={task.id}>
+                  <td>{task.title}</td>
+                  <td>{task.description}</td>
+                  <td>{task.startDate}</td>
+                  <td style={{ display: "flex", gap: "20px" }}>
+                    <button style={{ backgroundColor: "blue" }} onClick={() => handleEditTask(task.id, task)}>Edit</button>
+                    <button style={{ backgroundColor: "red" }} onClick={() => handleDeleteTask(task.id)}>Delete</button>
+                    <button onClick={() => handleIncompleteTask(task.id)}>Mark as Incomplete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
     </div>
   );
